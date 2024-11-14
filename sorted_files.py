1759aaf4dfc68fb5578b2ce6d783609cb87a642f
Nikolay Teplykh <wettak@yandex.ru>
2024-11-15 00:04:33 +0800

    added nothing

--- sorted_files.py
+++ sorted_files.py
@@ -2,11 +2,11 @@ files = ('1.txt', '2.txt', '3.txt')
 
 len_file = {}
 data = {}
-for file in files:    
+for file in files:
     with open(file, 'r', encoding='UTF-8') as f:
         len_file[file] = len(f.readlines())
-        print(f.read())
-        f.flush()
+        # print(f.read())
+        # f.flush()
 
 with open('result.txt', 'a', encoding='UTF-8') as f:
     sorted_dict = dict(sorted(len_file.items(), key=lambda item: item[1]))
