5dea8eea71123488ee74e2f69764e5eb12dda219
Nikolay Teplykh <wettak@yandex.ru>
2024-11-15 10:58:42 +0800

    the homework in done

--- sorted_files.py
+++ sorted_files.py
@@ -1,17 +1,21 @@
 files = ('1.txt', '2.txt', '3.txt')
-
 len_file = {}
 data = {}
+
 for file in files:
+    try:
+        with open(file, 'r', encoding='UTF-8') as f:
+            len_file[file] = len(f.readlines())
+
+    except FileNotFoundError:
+        print(f"File {file} Not Found")
+
     with open(file, 'r', encoding='UTF-8') as f:
-        len_file[file] = len(f.readlines())
-        # print(f.read())
-        # f.flush()
+        data[file] = f.read()
 
 with open('result.txt', 'a', encoding='UTF-8') as f:
     sorted_dict = dict(sorted(len_file.items(), key=lambda item: item[1]))
-    for key, value in sorted_dict.items():
-        f.write(key + '\n')
-        f.write(str(value) + '\n')
-        # f.write(data[key])
+
+    for file_name, num_str in sorted_dict.items():
+        f.write(file_name + '\n' + str(num_str) + '\n' + data[file_name] + '\n')
 
